6917a8a4855d3f9d33e59afc1f2767fa0a9c8638
wanko <kouji124@w4.dion.ne.jp>
2021-03-28 23:44:17 -0700

    separated front-back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react'
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message));
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>フロントエンド</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
