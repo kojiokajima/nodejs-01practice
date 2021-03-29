779052f8bea3e1fb77b7616b54d46d6be8f81241
wanko <kouji124@w4.dion.ne.jp>
2021-03-29 13:17:20 -0700

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
+
+const connection = mysql.createConnection({
+    host: process.env.NODE_HOST,
+    port: process.env.NODE_PORT,
+    user: process.env.NODE_USER,
+    password: process.env.NODE_PASSWORD,
+    database: process.env.NODE_DATABSE
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log('error occured ', err.stack);
+        return;
+    }
+    console.log('Connection established')
+}) 
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -12,4 +30,10 @@ app.get('/api', (req, res) => {
 
 app.listen(port, (req, res) => {
     console.log(`listening on ${port}`)
+    console.log(process.env.NODE_HOST)
+    console.log(process.env.NODE_USER)
+    console.log(process.env.NODE_PASSWORD)
+    console.log(process.env.NODE_DATABASE)
+    console.log(process.env.NODE_PORT)
+
 })
