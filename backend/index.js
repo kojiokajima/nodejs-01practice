19d503db62de02bd48e3c48a074a67409ea88841
wanko <kouji124@w4.dion.ne.jp>
2021-03-29 22:31:19 -0700

    connected to cleardb

--- backend/index.js
+++ backend/index.js
@@ -5,26 +5,93 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+// ---------------------------------
+const tableName = 'users';
+// ---------------------------------
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 // app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 const connection = mysql.createConnection({
-    host: process.env.NODE_HOST,
-    port: process.env.NODE_PORT,
     user: process.env.NODE_USER,
     password: process.env.NODE_PASSWORD,
-    // database: process.env.NODE_DATABSE
-    database: 'nodejs_01practice'
+    host: process.env.NODE_HOST,
+    // database: process.env.NODE_DATABSE,
+    database: 'heroku_530121ea020abc8',
+    port: process.env.NODE_PORT,
+    // database: 'nodejs_01practice'
 })
 
-connection.connect((err, results) => {
-    if (err) {
-        console.log('error occured ', err.stack);
+// connection.connect((err, results) => {
+//     if (err) {
+//         console.log('error occured ', err.stack);
+//         return;
+//     }
+//     console.log('Connection established')
+//     console.log(__dirname)
+// }) 
+
+// ---------------------------------
+connection.connect((error, results) => {
+    if (error) {
+        console.log('error occured ', error.stack);
         return;
     }
     console.log('Connection established')
-    console.log(__dirname)
-}) 
+    // console.log('results: ', results)
+    // console.log(__dirname)
+})
+
+connection.query(
+    'CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, f_name TEXT, l_name TEXT, email TEXT, password TEXT, PRIMARY KEY (id));',
+    (error, response) => {
+        if (error) {
+            console.log('ERROR CREATING TABLE')
+            console.log('error: ', error);
+        }
+    }
+);
+
+// connection.query(
+//     'SHOW FIELDS FROM users;',
+//     (error, response) => {
+//     console.log('response: ', response);
+// });
+
+connection.query(
+    "INSERT IGNORE INTO users VALUES (1, 'test1', 'user1', 'test1@gmail.com', 'test1');",
+    (err, results) => {
+        if (err) {
+            console.log("DATA WAS NOT INSERTED")
+        } else {
+            console.log('resultssss: ', results)
+        }
+    }
+)
+
+connection.query(
+    'SHOW tables',
+    (err, results) => {
+        if (err) {
+            console.log("CANT SHOW")
+        } else {
+            console.log(results)
+        }
+    }
+)
+
+connection.query(
+    'SELECT * FROM users;',
+    (err, results) => {
+        if (err) {
+            console.log('NO DATA WAS SELECTED');
+        } else {
+            console.log("SELECT: ", results)
+        }
+    }
+)
+
+// ---------------------------------
 
 // app.get('/', (req, res) => {
 //     connection.query(
